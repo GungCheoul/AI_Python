5760dec91554e3dcdd84b3090c6148d3071bfb76
GungCheoul <jkc3556@gmail.com>
2021-03-07 16:18:31 +0900

    practice - chatbot engine creating vocab dictionary for model

---
+++ train_tools/dict/create_dict.py
@@ -0,0 +1,33 @@
+# 엔진. 의도 분류 및 개체명 인식 모델의 학습을 위한 단어 사전 생성
+
+from utils.preprocess import Preprocess
+from tensorflow.keras import preprocessing
+import pickle
+
+
+def read_corpus_data(filename):
+    with open(filename, 'r', encoding='UTF-8') as f:
+        data = [line.split('\t') for line in f.read().splitlines()]
+        data = data[1:]
+    return data
+
+corpus_data = read_corpus_data('./corpus.txt')
+
+p = Preprocess()
+dict = []
+for c in corpus_data:
+    pos = p.pos(c[1])
+    for k in pos:
+        dict.append(k[0])
+
+tokenizer = preprocessing.text.Tokenizer(oov_token='OOV')
+tokenizer.fit_on_texts(dict)
+word_index = tokenizer.word_index
+
+f = open('chatbot_dict.bin', 'wb')
+try:
+    pickle.dump(word_index, f)
+except Exception as e:
+    print(e)
+finally:
+    f.close()
